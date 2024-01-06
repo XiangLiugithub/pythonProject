174e343e5b5ff576cfb01fff41b840027c0db033
Xiang Liu <xliu@mail.bradley.edu>
2024-01-06 12:37:23 -0600

    Added max number of guesses before losing

--- Hangman_Challenge.py
+++ Hangman_Challenge.py
@@ -9,14 +9,22 @@ for letter in secret_word:
     display_word += '_'
 print(display_word)
 
-
+num = 0
 game_over = False
+
 while not game_over:
     guess = input("Guess a letter ").lower()
     for position in range(len(secret_word)):
         letter = secret_word[position]
         if letter == guess:
             display_word[position] = letter
+    if guess not in secret_word:
+        num += 1
+        guesses_left = 5 - num
+        print(f"You have {guesses_left} guesses left")
+        if num >= 5:
+            print("You lose")
+            game_over = True
     print(display_word)
 
     if '_' not in display_word:
